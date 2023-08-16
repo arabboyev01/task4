7f0c18c9e78304c544c47b2e7eea8cd254350b9f
arabboyev01 <95698940+arabboyev01@users.noreply.github.com>
2023-08-16 20:58:17 +0500

    Login and sign up pages

--- src/components/Users/SignUp/index.tsx
+++ src/components/Users/SignUp/index.tsx
@@ -2,22 +2,21 @@ import DumbSignUp from '@/components/Users/SignUp/DumbSignUp'
 import { SignUpFormData } from '@/types'
 import { useRouter } from 'next/router'
 import { api } from '@/config'
-import { useState } from "react"
+import { useState } from 'react'
 
 const SignUp = () => {
 
     const router = useRouter()
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState<boolean>(false)
+    const handleSwitch = () => router.push('/login')
     const handleSignUp = (values: SignUpFormData) => {
         setLoading(true)
-        api.SignUp("api/sign-up", values).then(data => {
+        api.SignUp('api/sign-up', values).then(data => {
             api.SetToken(data.token)
-            router.push("/")
+            router.push('/')
             setLoading(false)
-        })
-            .catch(err => console.log(err)).finally(() => setLoading(false))
+        }).catch(err => console.log(err)).finally(() => setLoading(false))
     }
-     const handleSwitch = () => router.push("/login")
 
     return <DumbSignUp handleSubmit={handleSignUp} handleSwitch={handleSwitch} loading={loading}/>
 }
